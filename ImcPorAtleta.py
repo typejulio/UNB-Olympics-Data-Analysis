224561cf3d7939c5f9b224e78a451e79bd9e4ac6
Julio Cesar <juliodourado419@gmail.com>
2023-12-04 19:28:48 -0300

    adicionando funcoes e testes

--- ImcPorAtleta.py
+++ ImcPorAtleta.py
@@ -1,4 +1,4 @@
-from math import isnan #Is Not A Number
+from math import isnan
 from dash.dependencies import Input, Output
 
 from pandas import read_csv
@@ -7,130 +7,182 @@ from dash import Dash, html, dcc
 
 from components import header as DefaultHeader
 
-dados = read_csv("dados/athlete_events.csv", delimiter = ",") #ler dados
+# Função auxiliar para converter medalhas em pontos
 
-# "ID"[0],
-# "Name"[1],
-# "Sex"[2],
-# "Age"[3],
-# "Height"[4],
-# "Weight"[5],
-# "Team"[6],
-# "NOC"[7],
-# "Games"[8],
-# "Year"[9],
-# "Season"[10],
-# "City"[11],
-# "Sport"[12],
-# "Event"[13],
-# "Medal"[14]
+
+def converter_medalha_em_pontos(medal):
+    if medal == "Gold":
+        return 3
+    elif medal == "Silver":
+        return 2
+    elif medal == "Bronze":
+        return 1
+    else:
+        return 0
+
+# Função auxiliar para verificar se um evento é um recorde mundial fictício
+
+
+def is_recorde_mundial(evento):
+    return "World Record" in evento
+
+# Função auxiliar para mapear gênero para contagem
+
+
+def mapear_genero(genero):
+    if genero == "M":
+        return "Masculino"
+    elif genero == "F":
+        return "Feminino"
+    else:
+        return "Outro"
+
+
+def contar_medalhas_por_pais(dados):
+    contagem_medalhas = dados.groupby("NOC")["Medal"].apply(
+        lambda x: sum(converter_medalha_em_pontos(m) for m in x)).to_dict()
+    return contagem_medalhas
+
+# verificar recorde mundial
+
+
+def verificar_recorde_mundial(dados):
+    recorde_mundial = any(is_recorde_mundial(evento)
+                          for evento in dados["Event"])
+    return recorde_mundial
+
+# contar atletas por gênero
+
+
+def contar_atletas_por_genero(dados):
+    dados["Genero"] = dados["Sex"].apply(mapear_genero)
+    contagem_genero = dados.groupby("Genero")["ID"].nunique().to_dict()
+    return contagem_genero
+
+
+def calcular_media_idade_atletas(dadosUnicosPorPessoa):
+    media_idade = calcular_media_idade(dadosUnicosPorPessoa)
+    return media_idade
+
+
+def calcular_media_idade(atletas):
+    if not atletas:
+        return 0
+    return sum(atleta["Age"] for atleta in atletas) / len(atletas)
+
+
+dados = read_csv("dados/athlete_events.csv", delimiter=",")
 
 dadosUnicosPorPessoa = []
 idsLidosPorAno = {}
 
-# Agrupaento dos dados
-#------------------------------------------------------------
+# Agrupamento dos dados
+# ------------------------------------------------------------
 for linha in dados.values:
-  if isnan(linha[4]) or isnan(linha[5]):
-    continue
+    if isnan(linha[4]) or isnan(linha[5]):
+        continue
 
-  id = linha[0] # Identificador Único
-  nome = linha[1]
-  altura = linha[4] / 100
-  peso = linha[5]
-  time = linha[6]
-  ano = linha[9]
+    id = linha[0]  # Identificador Único
+    nome = linha[1]
+    altura = linha[4] / 100
+    peso = linha[5]
+    time = linha[6]
+    ano = linha[9]
 
-  if idsLidosPorAno.get(ano) == None:
-    idsLidosPorAno[ano] = set()
-  elif id in idsLidosPorAno[ano]:
-    continue
-  
-  imc = peso/(altura**2)
+    if idsLidosPorAno.get(ano) == None:
+        idsLidosPorAno[ano] = set()
+    elif id in idsLidosPorAno[ano]:
+        continue
 
-  dadosUnicosPorPessoa.append({"nome": nome, "imc": imc, "ano": ano})
-  idsLidosPorAno[ano].add(id)
-#------------------------------------------------------------
+    imc = peso / (altura ** 2)
+
+    dadosUnicosPorPessoa.append({"nome": nome, "imc": imc, "ano": ano})
+    idsLidosPorAno[ano].add(id)
+# ------------------------------------------------------------
 
 # Ordenação dos dados por IMC
-#------------------------------------------------------------
-dadosUnicosPorPessoa.sort(key=lambda item: item["imc"]) # Ordena o array de acordo com o imc
-#------------------------------------------------------------
+# ------------------------------------------------------------
+# Ordena o array de acordo com o imc
+dadosUnicosPorPessoa.sort(key=lambda item: item["imc"])
+# ------------------------------------------------------------
 
-# Criação do DataFrame
-#------------------------------------------------------------
+# Criação do DataFrame agrupado por ano
+# ------------------------------------------------------------
 dataFrameAgrupadoPorAno = {}
 for dado in dadosUnicosPorPessoa:
-  ano = dado["ano"]
-  nome = dado["nome"]
-  imc = dado["imc"]
+    ano = dado["ano"]
+    nome = dado["nome"]
+    imc = dado["imc"]
 
-  if dataFrameAgrupadoPorAno.get(ano) == None:
-    dataFrameAgrupadoPorAno[ano] = { "nome":[], "imc": []}
-  
-  dataFrameAgrupadoPorAno[ano]["nome"].append(nome)
-  dataFrameAgrupadoPorAno[ano]["imc"].append(imc)
-#------------------------------------------------------------
+    if dataFrameAgrupadoPorAno.get(ano) == None:
+        dataFrameAgrupadoPorAno[ano] = {"nome": [], "imc": []}
+
+    dataFrameAgrupadoPorAno[ano]["nome"].append(nome)
+    dataFrameAgrupadoPorAno[ano]["imc"].append(imc)
+# ------------------------------------------------------------
 
 # Ordenação dos anos de forma decrescente
-#------------------------------------------------------------
+# ------------------------------------------------------------
 anos = list(dataFrameAgrupadoPorAno.keys())
-anos.sort(reverse=True) # Ordena os anos de forma decrescente
-#------------------------------------------------------------
+anos.sort(reverse=True)  # Ordena os anos de forma decrescente
+# ------------------------------------------------------------
 
-# Criação do layout (vizualização)
-#------------------------------------------------------------
+# Criação do layout (visualização)
+# ------------------------------------------------------------
 app = Dash(__name__)
 app.layout = html.Div(
-  className="container",
-  children=[
-    DefaultHeader,
-    html.H1("IMC por Atleta"),
-    dcc.Dropdown(
-      id="anoSelecionado",
-      options=[{ "label": str(ano), "value": ano } for ano in anos],
-      value=anos[0],
-      style={"width": "100px"}
-    ),
-    dcc.RadioItems(
-      id="ordemSelecionada",
-      options=[
-        {"label": "Decrescente", "value": "maior"},
-        {"label": "Crescente", "value": "menor"}
-      ],
-      value="menor"
-    ),
-    dcc.Graph(
-      id="grafico",
-      figure=[]
-    )
-  ]
+    className="container",
+    children=[
+        DefaultHeader,
+        html.H1("IMC por Atleta"),
+        dcc.Dropdown(
+            id="anoSelecionado",
+            options=[{"label": str(ano), "value": ano} for ano in anos],
+            value=anos[0],
+            style={"width": "100px"},
+        ),
+        dcc.RadioItems(
+            id="ordemSelecionada",
+            options=[
+                {"label": "Decrescente", "value": "maior"},
+                {"label": "Crescente", "value": "menor"},
+            ],
+            value="menor",
+        ),
+        dcc.Graph(
+            id="grafico",
+            figure=[],
+        ),
+    ]
 )
-#------------------------------------------------------------
+# ------------------------------------------------------------
 
 # Criação do callback (função que é chamada sempre que um dos inputs são alterados)
-#------------------------------------------------------------
+# ------------------------------------------------------------
+
+
 @app.callback(
-  Output(component_id="grafico", component_property="figure"),
-  [
-    Input(component_id="anoSelecionado", component_property="value"), 
-    Input(component_id="ordemSelecionada", component_property="value")
-  ]
+    Output(component_id="grafico", component_property="figure"),
+    [
+        Input(component_id="anoSelecionado", component_property="value"),
+        Input(component_id="ordemSelecionada", component_property="value"),
+    ],
 )
 def atualizarGrafico(anoSelecionado, ordemSelecionada):
-  df = dataFrameAgrupadoPorAno[anoSelecionado].copy()
-  if ordemSelecionada == "menor":
-    df["nome"] = df["nome"][0:10]
-    df["imc"] = df["imc"][0:10]
-  else:
-    df["nome"] = df["nome"][::-1][0:10]
-    df["imc"] = df["imc"][::-1][0:10]
+    df = dataFrameAgrupadoPorAno[anoSelecionado].copy()
+    if ordemSelecionada == "menor":
+        df["nome"] = df["nome"][0:10]
+        df["imc"] = df["imc"][0:10]
+    else:
+        df["nome"] = df["nome"][::-1][0:10]
+        df["imc"] = df["imc"][::-1][0:10]
 
-  grafico = px.bar(df, x="nome", y="imc")
-  return grafico
-#------------------------------------------------------------
+    grafico = px.bar(df, x="nome", y="imc")
+    return grafico
+# ------------------------------------------------------------
+
 
 # Rodar o servidor
-#------------------------------------------------------------
+# ------------------------------------------------------------
 app.run_server(port=3003)
-#------------------------------------------------------------
+# ------------------------------------------------------------
